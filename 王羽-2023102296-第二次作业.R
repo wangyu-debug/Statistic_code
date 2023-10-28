aaa84c9c7ebfcc0b37602eb05fb1223d6213247a
wangyu-debug <70136801+wangyu-debug@users.noreply.github.com>
2023-10-28 17:04:32 +0800

    ***

--- 王羽-2023102296-第二次作业.R
+++ 王羽-2023102296-第二次作业.R
@@ -1,1 +1,23 @@
-##
+##eg1:weibull distribution
+u = runif(10000)
+lamda = 1/2 
+k = 1/2
+x1 = lamda*(-log(1-u))^(1/k)
+hist(x1,prob = TRUE)
+xx1 = seq(0,50,0.8)
+lines(xx1,(k/lamda)*((xx1/lamda)^(k-1))*exp(-(xx1/lamda)^k))  # 绘制密度函数，与频数分布直方图的趋势作比较
+
+##eg2:Cauthy distribution
+
+
+
+
+
+
+
+
+
+
+
+
+
